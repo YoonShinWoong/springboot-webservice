26a6a96d9e660bd560f58159871732b0b0d2344f
bale <bale@daangn.com>
2021-12-05 12:34:06 +0900

    :card_file_box: add Post model

---
+++ src/main/java/com/bale/study/springboot/domain/posts/PostsRepository.java
@@ -0,0 +1,6 @@
+package com.bale.study.springboot.domain.posts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostsRepository extends JpaRepository<Posts, Long> {
+}
---
+++ src/test/java/com/bale/study/springboot/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,46 @@
+package com.bale.study.springboot.domain.posts;
+
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PostsRepositoryTest {
+
+    @Autowired
+    PostsRepository postsRepository;
+
+    @After
+    public void cleanup() {
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void 게시글저장_불러오기() {
+        //given
+        String title = "첫 게시글이에요~";
+        String content = "안녕하세요!";
+
+        postsRepository.save(Posts.builder()
+                .title(title)
+                .content(content)
+                .author("bale@gmail.com")
+                .build());
+
+        //when
+        List<Posts> postsList = postsRepository.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+        assertThat(posts.getTitle()).isEqualTo(title);
+        assertThat(posts.getContent()).isEqualTo(content);
+    }
+}
