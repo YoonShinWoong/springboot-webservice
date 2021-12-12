9293c2a3ed739dcee22d9c4e5ff4b438e236c243
bale <bale@daangn.com>
2021-12-12 18:41:55 +0900

    :sparkles: add Posts findAll feature

--- src/main/java/com/bale/study/springboot/service/posts/PostsService.java
+++ src/main/java/com/bale/study/springboot/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.bale.study.springboot.service.posts;
 
 import com.bale.study.springboot.domain.posts.Posts;
 import com.bale.study.springboot.domain.posts.PostsRepository;
+import com.bale.study.springboot.web.dto.PostsListResponseDto;
 import com.bale.study.springboot.web.dto.PostsResponseDto;
 import com.bale.study.springboot.web.dto.PostsSaveRequestDto;
 import com.bale.study.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -35,5 +39,12 @@ public class PostsService {
                 .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
--- src/main/java/com/bale/study/springboot/web/PostsApiController.java
+++ src/main/java/com/bale/study/springboot/web/PostsApiController.java
@@ -1,10 +1,12 @@
 package com.bale.study.springboot.web;
 
 import com.bale.study.springboot.service.posts.PostsService;
+import com.bale.study.springboot.web.dto.PostsListResponseDto;
 import com.bale.study.springboot.web.dto.PostsResponseDto;
 import com.bale.study.springboot.web.dto.PostsSaveRequestDto;
 import com.bale.study.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
