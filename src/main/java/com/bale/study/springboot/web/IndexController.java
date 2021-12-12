cf45a3d533cdf9bed0c82dbfeec886a740c02240
bale <bale@daangn.com>
2021-12-12 16:44:58 +0900

    :sparkles: add POST CREATE feature

--- src/main/java/com/bale/study/springboot/web/IndexController.java
+++ src/main/java/com/bale/study/springboot/web/IndexController.java
@@ -6,8 +6,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class IndexController {
 
-    @GetMapping
+    @GetMapping("/")
     public String index(){
         return "index";
+    }
+
+    @GetMapping("/posts/save")
+    public String postSave() {
+        return "posts-save";
     }
 }
